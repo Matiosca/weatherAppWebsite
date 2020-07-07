3eb0f5fd777711d2f24995c3f76423b1465333ba
Mattia <gandola.mattia@gmail.com>
2020-07-07 20:49:14 +0100

    mobile responsive progress
    Missing hamburger menu for navbar

---
+++ overflowElement.js
@@ -0,0 +1,7 @@
+var docWidth = document.documentElement.offsetWidth;
+
+[].forEach.call(document.querySelectorAll("*"), function (el) {
+  if (el.offsetWidth > docWidth) {
+    console.log(el);
+  }
+});
